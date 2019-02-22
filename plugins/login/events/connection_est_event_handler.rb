a3cc895a5a910ada63810187fda6fccbb0020705
MarsGrad <MarsGrad@users.noreply.github.com>
2019-02-22 00:54:13 -0500

    Adding random connect screen

--- plugins/login/events/connection_est_event_handler.rb
+++ plugins/login/events/connection_est_event_handler.rb
@@ -1,12 +1,12 @@
 module AresMUSH
   module Login
-    class ConnectionEstablishedEventHandler    
+    class ConnectionEstablishedEventHandler
       def on_event(event)
         client = event.client
-        
+
         # Connect screen ansi
-        filename = File.join(AresMUSH.game_path, 'text', 'connect.txt')        
-        
+        filename = File.join(AresMUSH.game_path, 'text', "#{[connect1.txt, connect2.txt, connect3.txt, connect4.txt, connect5.txt].sample}")
+
         if (File.exist?(filename))
           client.emit File.read(filename, :encoding => "UTF-8")
         else
@@ -19,5 +19,5 @@ module AresMUSH
     end
   end
 end
-    
+
     
