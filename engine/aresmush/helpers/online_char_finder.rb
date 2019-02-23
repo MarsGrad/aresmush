b410af8727d709ec563847db5fa1aa9d32df0a92
MarsGrad <MarsGrad@users.noreply.github.com>
2019-02-23 07:44:00 -0500

    Revert "Adding colorizer cmd and admin_name attrib"
    This reverts commit 3fe20ef04766a02fbd3a9becd6dabca8d208c596.

--- engine/aresmush/helpers/online_char_finder.rb
+++ engine/aresmush/helpers/online_char_finder.rb
@@ -1,5 +1,5 @@
 module AresMUSH
-  # Searches for online characters. Uses partial name matches.  Resulting 'target' will be an
+  # Searches for online characters. Uses partial name matches.  Resulting 'target' will be an 
   # OnlineCharResult contianing both the character and their client.
   class OnlineCharResult
     attr_accessor :client, :char
@@ -8,7 +8,7 @@ module AresMUSH
       @char = char
     end
   end
-
+  
   class OnlineCharFinder
     # @return [FindResult] Where FindResult's 'target' is an OnlineCharResult
     def self.find(name)
@@ -20,7 +20,7 @@ module AresMUSH
         .select { |other_client, other_char| partial_match?(other_char, name)}
         .map { |other_client, other_char| OnlineCharResult.new(other_client, other_char )}
       end
-
+      
       if (online.count == 0)
         if (Character.all.select { |c| c.name_upcase.start_with?(name.upcase) }.first)
           return FindResult.new(nil, t('db.no_char_online_found', :name => name))
@@ -33,10 +33,10 @@ module AresMUSH
       FindResult.new(nil, t('db.ambiguous_char_online', :name => name))
     end
 
-    # @yieldparam result [OnlineCharResult]
+    # @yieldparam result [OnlineCharResult] 
     def self.with_an_online_char(name, client, &block)
       result = self.find(name)
-
+              
       if (!result.found?)
         client.emit_failure(result.error)
         return
@@ -44,13 +44,13 @@ module AresMUSH
 
       yield result.target
     end
-
+        
     # @yieldparam results [OnlineCharResult]
     def self.with_online_chars(names, client, &block)
       to_clients = []
       names.each do |name|
         result = self.find(name)
-
+                
         if (!result.found?)
           client.emit_failure(result.error)
           return
@@ -59,25 +59,23 @@ module AresMUSH
       end
       yield to_clients
     end
-
+    
     private
-
+    
     def self.exact_match?(char, name)
       name = name.upcase
       return false if name.blank?
       return false if !char
       return true if char.name_upcase == name
       return true if char.alias_upcase == name
-      return true if char.admin_name_upcase == name
       return false
     end
-
+    
     def self.partial_match?(char, name)
       name = name.upcase
       return false if name.blank?
       return false if !char
       return true if char.name_upcase.start_with?(name)
-      return true if char.admin_name_upcase.start_with?(name)
       return false
     end
   end
