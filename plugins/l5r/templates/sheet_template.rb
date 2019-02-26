b3a100b37798879438b812fb79c7111447709410
MarsGrad <MarsGrad@users.noreply.github.com>
2019-02-26 00:54:14 -0500

    Update sheet_template.rb

--- plugins/l5r/templates/sheet_template.rb
+++ plugins/l5r/templates/sheet_template.rb
@@ -32,7 +32,7 @@ module AresMUSH
         fire = "#{L5R.calc_l5r_ring(char, 'fire')}"
         fire << "\n"
         traits = char.l5r_traits.select { |t| t.name == 'agility' || t.name == 'intelligence' }
-        trait1, trait2 = traits[0], trait[1]
+        trait1, trait2 = traits[0], traits[1]
         fire << left("%xr#{trait1.name}%xn: #{trait1.rank}%t")
         fire << left("%xr#{trait2.name}%xn: #{trait2.rank}")
         fire
