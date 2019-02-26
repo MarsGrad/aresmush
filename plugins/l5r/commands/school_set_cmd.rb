f35d3a18b0343db169cb6a158764e0fb60c9579f
MarsGrad <MarsGrad@users.noreply.github.com>
2019-02-26 02:30:38 -0500

    Update school_set_cmd.rb

--- plugins/l5r/commands/school_set_cmd.rb
+++ plugins/l5r/commands/school_set_cmd.rb
@@ -15,36 +15,36 @@ module AresMUSH
           self.target_name = enactor_name
           self.family_name = titlecase_arg(cmd.args)
         end
+      end
 
-        def required_args
-          [self.target_name, self.school_name]
-        end
+      def required_args
+        [self.target_name, self.school_name]
+      end
 
-        def check_valid_school
-          return t('l5r.invalid_school') if L5R.is_valid_school?(self.school_name)
-          return nil
-        end
+      def check_valid_school
+        return t('l5r.invalid_school') if L5R.is_valid_school?(self.school_name)
+        return nil
+      end
 
-        def check_can_set
-          return nil if enactor_name == self.target_name
-          return nil if L5R.can_manage_abilities?(enactor)
-          return t('dispatcher.not_allowed')
-        end
+      def check_can_set
+        return nil if enactor_name == self.target_name
+        return nil if L5R.can_manage_abilities?(enactor)
+        return t('dispatcher.not_allowed')
+      end
 
-        def check_chargen_locked
-          return nil if L5R.can_manage_abilities?(enactor)
-          Chargen.check_chargen_locked(enactor)
-        end
+      def check_chargen_locked
+        return nil if L5R.can_manage_abilities?(enactor)
+        Chargen.check_chargen_locked(enactor)
+      end
 
-        def handle
-          ClassTargetFinder.with_a_character(self.target_name, client, enactor) do |model|
+      def handle
+        ClassTargetFinder.with_a_character(self.target_name, client, enactor) do |model|
 
-            school_config = L5R.find_school_config(self.school_name)
+          school_config = L5R.find_school_config(self.school_name)
 
-            school = school_config['name'].downcase
-            clan = school_config['clan'].downcase
-            trait_bonus = school_config['clan']
-        end
+          school = school_config['name'].downcase
+          clan = school_config['clan'].downcase
+          trait_bonus = school_config['clan']
       end
     end
   end
