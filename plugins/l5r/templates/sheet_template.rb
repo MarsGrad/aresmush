8b98af254489254aba2ff5d624d04e012163c101
MarsGrad <MarsGrad@users.noreply.github.com>
2019-02-26 14:45:59 -0500

    sheet format changes

--- plugins/l5r/templates/sheet_template.rb
+++ plugins/l5r/templates/sheet_template.rb
@@ -54,32 +54,41 @@ module AresMUSH
         fire
       end
 
-      def air_ring
-        air = "%xhAir Ring%xn: #{L5R.calc_l5r_ring(char, 'air')}"
-        air << "\n"
+      def air_ring_title
+        air = "Air Ring: #{L5R.calc_l5r_ring(char, 'air')}"
+        air
+      end
+
+      def air_ring_traits
         traits = char.l5r_traits.select { |t| t.name == 'reflexes' || t.name == 'awareness' }
         trait1, trait2 = traits[0], traits[1]
-        air << left("%xh#{trait1.name.titlecase}%xn: #{trait1.rank}%t", 30)
+        air = left("%xh#{trait1.name.titlecase}%xn: #{trait1.rank}%t", 30)
         air << left("%xh#{trait2.name.titlecase}%xn: #{trait2.rank}", 30)
         air
       end
 
-      def water_ring
-        water = "%x27Water Ring%xn: #{L5R.calc_l5r_ring(char, 'water')}"
-        water << "\n"
+      def water_ring_title
+        water = "Water Ring: #{L5R.calc_l5r_ring(char, 'water')}"
+        water
+      end
+
+      def water_ring_traits
         traits = char.l5r_traits.select { |t| t.name == 'strength' || t.name == 'perception' }
         trait1, trait2 = traits[0], traits[1]
-        water << left("%x27#{trait1.name.titlecase}%xn: #{trait1.rank}%t", 30)
+        water = left("%x27#{trait1.name.titlecase}%xn: #{trait1.rank}%t", 30)
         water << left("%x27#{trait2.name.titlecase}%xn: #{trait2.rank}", 30)
         water
       end
 
-      def earth_ring
-        earth = "%x3Earth Ring%xn: #{L5R.calc_l5r_ring(char, 'earth')}"
-        earth << "\n"
+      def earth_ring_title
+        earth = "Earth Ring: #{L5R.calc_l5r_ring(char, 'earth')}"
+        earth
+      end
+
+      def earth_ring_traits
         traits = char.l5r_traits.select { |t| t.name == 'stamina' || t.name == 'willpower' }
         trait1, trait2 = traits[0], traits[1]
-        earth << left("%x3#{trait1.name.titlecase}%xn: #{trait1.rank}%t", 30)
+        earth = left("%x3#{trait1.name.titlecase}%xn: #{trait1.rank}%t", 30)
         earth << left("%x3#{trait2.name.titlecase}%xn: #{trait2.rank}", 30)
         earth
       end
