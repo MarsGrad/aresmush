5be72351e1a866a6d7b78c5591609b6254d9cbb7
MarsGrad <MarsGrad@users.noreply.github.com>
2019-02-26 05:02:47 -0500

    Update helpers.rb

--- plugins/l5r/helpers.rb
+++ plugins/l5r/helpers.rb
@@ -65,7 +65,7 @@ module AresMUSH
 
     def self.find_school_config(school_name)
       return nil if !school_name
-      schools = Global.read_config('l5r,' 'schools')
+      schools = Global.read_config('l5r', 'schools')
       schools.select { |s| s['name'].downcase == school_name.downcase }.first
     end
 
