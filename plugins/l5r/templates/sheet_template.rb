3b0e741a4c8d5da12e3486e579f64f9d0ef2cb68
MarsGrad <MarsGrad@users.noreply.github.com>
2019-02-25 23:25:56 -0500

    Update sheet_template.rb

--- plugins/l5r/templates/sheet_template.rb
+++ plugins/l5r/templates/sheet_template.rb
@@ -24,11 +24,11 @@ module AresMUSH
       end
 
       def rings
-        fire = char.calc_l5r_ring(fire)
-        air = char.calc_l5r_ring(air)
-        water = char.calc_l5r_ring(water)
-        earth = char.calc_l5r_ring(earth)
-        void = char.l5r_void_ring
+        fire = L5R.calc_l5r_ring(char, 'fire')
+        air = L5R.calc_l5r_ring(char, 'air')
+        water = L5R.calc_l5r_ring(char, 'water')
+        earth = L5R.calc_l5r_ring(char, 'earth')
+        void = L5R.l5r_void_ring
 
         rings = "Fire: #{fire}%tAir: #{air}%tWater: #{water}%tEarth: #{earth}%tVoid: #{void}"
       end
