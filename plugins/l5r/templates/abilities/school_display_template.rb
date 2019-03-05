27d8e5c61bc2560787f0b1f6b1826c3441474ebb
MarsGrad <MarsGrad@users.noreply.github.com>
2019-03-05 09:45:38 -0500

    Update school_display_template.rb

--- plugins/l5r/templates/abilities/school_display_template.rb
+++ plugins/l5r/templates/abilities/school_display_template.rb
@@ -11,14 +11,14 @@ module AresMUSH
       def name_clan
         name = school_config['name']
         clan = school_config['clan']
-        display = left("Name: #{name}", 30)
-        display << "Clan: #{clan}"
+        display = left("%xyName%xn: #{name}", 30)
+        display << "%xyClan%xn: #{clan}"
         display
       end
 
       def trait
         trait = school_config['trait_bonus']
-        display = "Trait Bonus: #{trait}"
+        display = "%xyTrait Bonus%xn: #{trait}"
         display
       end
 
@@ -26,13 +26,13 @@ module AresMUSH
         skills = school_config['skills']
         skill_choice = school_config['skill_choice']
         skills = skills.join(", ")
-        display = "Skills: #{skills}, and #{skill_choice}"
+        display = "%xySkills%xn: #{skills}, and #{skill_choice}"
         display
       end
 
       def honor
         honor = school_config['honor']
-        display = "Honor: #{honor}"
+        display = "%xyHonor%xn: %xr#{honor}%xn"
         display
       end
 
@@ -40,7 +40,7 @@ module AresMUSH
         techs = school_config['techniques']
         first = techs.shift(3)
 
-        display = "Techniques: "
+        display = "%xyTechniques%xn: "
         display << first.join(", ")
         display << "%r"
         display << techs.join(", ")
@@ -50,21 +50,21 @@ module AresMUSH
 
       def starting_tech
         tech = school_config['starting_technique']
-        display = "Technique: #{tech}"
+        display = "%xyTechnique%xn: #{tech}"
         display
       end
 
       def affinity_deficiency
         affinity = school_config['affinity']
         deficiency = school_config['deficiency']
-        display = left("Affinity: #{affinity}", 30)
-        display << "Deficiency: #{deficiency}"
+        display = left("%xyAffinity%xn: #{affinity}", 30)
+        display << "%xyDeficiency%xn: #{deficiency}"
         display
       end
 
       def spells
         spells = school_config['spells']
-        display = "Starting Spells: #{spells}"
+        display = "%xyStarting Spells%xn: #{spells}"
         display
       end
     end
