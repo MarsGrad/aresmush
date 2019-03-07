6e7205383f4340dc4030b4ff93e0669c96a13d51
MarsGrad <MarsGrad@users.noreply.github.com>
2019-03-07 12:18:15 -0500

    Update sheet_init_cmd.rb

--- plugins/l5r/commands/sheet_init_cmd.rb
+++ plugins/l5r/commands/sheet_init_cmd.rb
@@ -114,8 +114,8 @@ module AresMUSH
         enactor.update(l5r_sheet_type: nil)
         enactor.update(l5r_stance: "attack")
         enactor.update(l5r_current_wounds: 0)
-        Demographics.set_group(model, "clan", "")
-        Demographics.set_group(model, "vocation", "")
+        Demographics.set_group(enactor, "clan", "")
+        Demographics.set_group(enactor, "vocation", "")
 
         client.emit_success t('l5r.init_success')
       end
