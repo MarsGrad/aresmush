2d3899eb24273aca8f42f0b9c34c48a114b0f4a8
MarsGrad <MarsGrad@users.noreply.github.com>
2019-03-08 03:54:34 -0500

    Revert "Update app_submit_cmd.rb"
    This reverts commit 3e6e562f4c99e198e36a8857cb248cc8e4e8c3e4.

--- plugins/chargen/commands/app_submit_cmd.rb
+++ plugins/chargen/commands/app_submit_cmd.rb
@@ -1,6 +1,7 @@
 module AresMUSH
   module Chargen
     class AppSubmitCmd
+      include L5R
       include CommandHandler
 
       attr_accessor :app_notes
@@ -15,8 +16,24 @@ module AresMUSH
       end
 
       def handle
+        school = enactor.l5r_current_school
+        school_config = L5R.find_school_config(school)
+        skills = school_config['skill_display']
+        spells = school_config['spells']
+        aff_def_choice = school_config['aff_def_choice']
+        choice = ""
+        if (skills)
+          choice << "%r%xySkills%xn: #{skills}"
+        end
+        if (spells)
+          choice << "%r%xySpells:%xn #{spells}"
+        end
+        if (aff_def_choice)
+          choice << "%r%xrNOTE:%xn Affinity and Deficiency need to be chosen."
+        end
+
         if (cmd.switch_is?("confirm"))
-          client.emit_success Chargen.submit_app(enactor, client.program[:app_notes])
+          client.emit_success Chargen.submit_app(enactor, client.program[:app_notes], choice)
           client.emit_ooc t('chargen.approval_reminder')
         else
           client.program[:app_notes] = self.app_notes
