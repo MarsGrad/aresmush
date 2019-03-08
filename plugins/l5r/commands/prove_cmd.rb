dc8a6e213c8e6e16f8e542e8bb08189db64282ab
MarsGrad <MarsGrad@users.noreply.github.com>
2019-03-08 06:16:16 -0500

    Update prove_cmd.rb

---
+++ plugins/l5r/commands/prove_cmd.rb
@@ -0,0 +1,48 @@
+module AresMUSH
+  module L5R
+    class ProveCmd
+      include CommandHandler
+
+      attr_accessor :ability_name
+
+      def parse_args
+        self.ability_name = downcase_arg(cmd.args)
+      end
+
+      def required_args
+        [self.ability_name]
+      end
+
+      def handle
+        case self.ability_name
+        when "fire"
+          found = L5R.calc_l5r_ring(enactor, 'fire')
+        when "air"
+          found = L5R.calc_l5r_ring(enactor, 'air')
+        when "earth"
+          found = L5R.calc_l5r_ring(enactor, 'earth')
+        when "water"
+          found = L5R.calc_l5r_ring(enactor, 'water')
+        when "void"
+          found = enactor.l5r_void_ring
+        when "insight"
+          found = L5R.calc_l5r_insight(enactor)
+        end
+
+        [ enactor.l5r_skills, enactor.l5r_traits, enactor.l5r_schools, enactor.l5r_kiho, enactor.l5r_kata, enactor.l5r_spells, enactor.l5r_advantages, enactor.l5r_disadvantages, enactor.l5r_techniques ].each do |list|
+          found = list.select { |a| a.name.downcase == self.ability_name }.first
+        end
+
+        if (!found)
+          client.emit_failure t('l5r.invalid_ability_name')
+          return
+        else
+          if (found.rank)
+            Rooms.emit_ooc_to_room enactor.room, t('l5r.prove_ranked', :character => enactor_name, :ability => found.name.titlecase, :rank => found.rank)
+          elsif (!found.rank)
+            Rooms.emit_ooc_to_room enactor.room, t('l5r.prove', :character => enactor_name, :ability => found.name.titlecase)
+        end
+      end
+    end
+  end
+end
